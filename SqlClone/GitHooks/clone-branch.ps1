b37ece41c7c27fb78dddffc77edb7cd682672afb
Owen Hall <owen.hall@red-gate.com>
2020-01-30 11:01:02 +0000

    Clone Branch script v0.2.
    Improved logging, extracted functions, improved structure.

--- SqlClone/GitHooks/clone-branch.ps1
+++ SqlClone/GitHooks/clone-branch.ps1
@@ -1,27 +1,22 @@
 ##########################################################################################
-# clone-branch.ps1 version 0.1
+############################## clone-branch.ps1 version 0.2 ##############################
 ##########################################################################################
 
-##########################################################################################
-# Configuration
-##########################################################################################
+##################################### Configuration ######################################
+
+#$VerbosePreference = "Continue"            # Uncomment this line to enable verbose logging
+$InformationPreference = "Continue"         # Comment this line to disable information logging
+$ErrorActionPreference = "Stop"
+
 $CloneServerUrl = "https://<clone-server-url>:14145"
 $ImageName = "<image-name>"                 # SQL Clone image from which dev database clones will be created
 $SqlServerMachineName = "<machine-name>"    # SQL Server machine hosting the clone database (must have Clone Agent installed):
 $SqlServerInstanceName = "<instance-name>"  # Use empty string for the default instance
-$DBName = "<clone-name>"                    # Database name linked to project (could pull this from project file XML if required) 
+$DBName = "<clone-name>"                    # Database name linked to project
+
 ##########################################################################################
 
-$ErrorActionPreference = "Stop"
-
-# This script currently assumes Windows Auth will be used to connect to the database clone
-
-# We start a timer so we can measure how long the provision takes
-$StopWatch = New-Object -TypeName System.Diagnostics.Stopwatch
-$StopWatch.Start()
-
-# Find the branch name we've switched to, which we use to label per-branch Clones
-if ($null -ne (Get-Command "git" -ErrorAction SilentlyContinue)) {
+function GetFromAndToBranchNames {
     $reflogResult = git reflog
     $lastReflog = $reflogResult[0]
     $lastReflogTokens = $lastReflog.Split(" ")
@@ -29,87 +24,87 @@ if ($null -ne (Get-Command "git" -ErrorAction SilentlyContinue)) {
     $toBranchName = $lastReflogTokens[7]
     $fromBranch = "$($fromBranchName | ForEach-Object {$_ -replace "\W"})"
     $toBranch = "$($toBranchName | ForEach-Object {$_ -replace "\W"})"
-    Write-Host "We have switched from branch ${fromBranch} to branch ${toBranch}"
-}
-else
-{
-    Write-Host "git could not be found, so from-branch and to-branch could not be determined"
-    Write-Host "This may go very badly, perhaps we should have terminated"
+
+    $fromBranch
+    $toBranch
 }
 
-# We will use the username and branch name to uniquify name the clones
-$username = $env:UserName
+function GetCloneBranchName {
+    param ($branchNameValue)
+    return "${DBName}_${branchNameValue}_${env:UserName}"
+}
 
-# Connect to Clone Server and obtain resources
-Connect-SqlClone $CloneServerUrl -ErrorAction Stop
-
-$image = Get-SqlCloneImage $ImageName -ErrorAction Stop
-Write-Host "Successfully found SQL Clone image $ImageName at $CloneServerUrl"
-
-$sqlServerInstance = Get-SqlCloneSqlServerInstance -MachineName $SqlServerMachineName -InstanceName $SqlServerInstanceName -ErrorAction Stop
-Write-Host "Found SQL Server instance ${SqlServerMachineName}\${SqlServerInstanceName}"
-
-function TrySaveBranchClone {
-    param ($branchnamevalue)
-    Write-Host "Rename $DBName with the branch and username so we can retrieve it later: ${DBName}_${branchnamevalue}_${username}"
-
-    $renameMe = Get-SqlClone -Name $DBName
-
-    try {
-        $renameOperation = Rename-SqlClone -Clone $renameMe -NewName  ${DBName}_${branchnamevalue}_${username}
-        Wait-SqlCloneOperation -Operation $renameOperation
-        return $true
-    }
-    catch {
-        # This could mask other errors - we are only considering renaming on switching to same branch
-        Write-Host "Skipped: The rename could not be completed, a checkout to the same branch may have occurred."
-        return $false
-    }
+function CloneExists {
+    param ($cloneName)
+    return $null -ne (Get-SqlClone -Name $cloneName -Location $sqlServerInstance -ErrorAction SilentlyContinue)
 }
 
 function CreateClone {
-    Write-Host "We create a new clone: ${DBName} and migrate the new clone to the version represented in the branch"   
+    Write-Verbose "Provisioning database ${DBName}..."
     New-SqlClone -Name ${DBName} -Image $image -Location $sqlServerInstance -ErrorAction Stop | Wait-SqlCloneOperation -ErrorAction Stop
 }
 
-# We create a new clone in the case we can't find a linked database clone, eg first time setup. 
-if ($null -eq (Get-SqlClone -Name $DBName -Location $sqlServerInstance -ErrorAction SilentlyContinue)) {
-    # Clone for current branch doesn't exist
-    Write-Host "Need to create a Clone $DBName as it doesn't exist already."
-    CreateClone
+function RenameClone {
+    param ($fromName, $toName)
+
+    Write-Verbose "Renaming $fromName to $toName..."
+    $renameMe = Get-SqlClone $fromName -ErrorAction Stop
+
+    $renameOperation = Rename-SqlClone -Clone $renameMe -NewName $toName
+    Wait-SqlCloneOperation -Operation $renameOperation -ErrorAction Stop
 }
 
-# We've switched branch so need to see if there's a clone already for this branch we can switch to
-$targetCloneName = "${DBName}_${toBranch}_${username}" # This is the clone name we're expecting
-Write-Host "Checking existence in SQL Clone for: $targetCloneName"
+function ProvisionCloneForBranch {
+    $fromCloneBranchName = GetCloneBranchName $fromBranch # This is what we will rename the current clone to, if present
+    $toCloneBranchName = GetCloneBranchName $toBranch # This is the clone name we're expecting
 
-if ($null -ne (Get-SqlClone -Name $targetCloneName -Location $sqlServerInstance -ErrorAction SilentlyContinue)) {
-    # Confirmed that Clone for branch exists so we save off the existing database
-    
-    if(-not (TrySaveBranchClone  $fromBranch))
-    {
-        return
+    # We create a new clone in the case we can't find a linked database clone, eg first time setup.
+    if (CloneExists $DBName) {
+        # Clone for current branch doesn't exist
+        RenameClone $DBName $fromCloneBranchName
     }
 
-    Write-Host "As $targetCloneName has been found, we rename this to be the linked dev database: $DBName"
-    $renameMe = Get-SqlClone $targetCloneName
-
-    $renameOperation = Rename-SqlClone -Clone $renameMe -NewName $DBName
-    Wait-SqlCloneOperation -Operation $renameOperation -ErrorAction Ignore
-}
-else {
-    # Per-branch Clone doesn't exist so we create a new one after save the current clone, naming it with the branch and user
-    Write-Host "Saved Clone $targetCloneName not found. Save current $fromBranch clone and create a new one."
-    if(-not (TrySaveBranchClone  $fromBranch))
-    {
-        return
+    # We've switched branch so need to see if there's a clone already for this branch we can switch to
+    if (CloneExists $toCloneBranchName) {
+        # Confirmed that clone for branch exists so we save off the existing database
+        RenameClone $toCloneBranchName $DBName
     }
-    CreateClone
+    else {
+        # Per-branch clone doesn't exist so we create a new one after save the current clone, naming it with the branch and user
+        CreateClone
+    }
 }
 
-# Stop the stopwatch
+##########################################################################################
+
+if ($null -eq (Get-Command "git" -ErrorAction SilentlyContinue)) {
+    throw "This script requires git to be installed and available on PATH"
+}
+
+$branchNames = GetFromAndToBranchNames
+$fromBranch = $branchNames[0]
+$toBranch = $branchNames[1]
+
+if ($fromBranch -eq $toBranch) {
+    return
+}
+
+Write-Information "Provisioning database ${DBName} for branch ${toBranch}..."
+Write-Verbose "Switched from branch ${fromBranch} to branch ${toBranch}"
+
+$StopWatch = New-Object -TypeName System.Diagnostics.Stopwatch
+$StopWatch.Start()
+
+Connect-SqlClone $CloneServerUrl -ErrorAction Stop
+
+$image = Get-SqlCloneImage $ImageName -ErrorAction Stop
+Write-Verbose "Found SQL Clone image $ImageName at $CloneServerUrl"
+
+$sqlServerInstance = Get-SqlCloneSqlServerInstance -MachineName $SqlServerMachineName -InstanceName $SqlServerInstanceName -ErrorAction Stop
+Write-Verbose "Found SQL Server instance ${SqlServerMachineName}\${SqlServerInstanceName}"
+
+ProvisionCloneForBranch
+
 $StopWatch.Stop()
 $provisionTime = $StopWatch.Elapsed.ToString('ss')
-Write-Host "Provisioned a database clone for branch ${toBranch}" -ForegroundColor Green
-if ($toBranch -ne $fromBranch) { Write-Host "(and saved off a clone for branch ${fromBranch} so we can switch back to it later)" -ForegroundColor Green }
-Write-Host "SCA Provision time: ${provisionTime} seconds" -ForegroundColor Green
+Write-Information "Database ${DBName} successfully provisioned by SQL Clone (took ${provisionTime} seconds)"
