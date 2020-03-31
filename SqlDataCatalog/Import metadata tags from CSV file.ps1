034acefe4e5ed79833b096e73ee4e5c78437aedd
Ian Johnson <ian.johnson@red-gate.com>
2020-03-31 15:13:06 +0100

    Importing from csv needs to find the column id

--- SqlDataCatalog/Import metadata tags from CSV file.ps1
+++ SqlDataCatalog/Import metadata tags from CSV file.ps1
@@ -15,23 +15,19 @@ Import-Module .\data-catalog.psm1 -Force
 # connect to your SQL Data Catalog instance - you'll need to generate an auth token in the UI
 Connect-SqlDataCatalog -AuthToken $authToken -ServerUrl $serverUrl
 
-$instances = Get-ClassificationInstance
-
-$instanceId = @( $instances | Where-Object { $_.Name -eq $instanceName })[0].InstanceId
 # Get the data from the .csv file
 $data = Import-Csv $csvPath |
     Select-Object Schema, Table, Column, "Information Type", "Sensitivity Label"
+
+$columns = Get-ClassificationColumn -InstanceName $instanceName -DatabaseName $databaseName
 
 for ($i = 0; $i -lt $data.Count; $i++) {
     if (-not $data[$i].Schema) {
         # Copy-down gaps in schema values, may not be needed in your scenario
         $data[$i].Schema = $data[$i - 1].Schema
     }
-    $data[$i] | Add-Member -MemberType AliasProperty -Name SchemaName -Value Schema
-    $data[$i] | Add-Member -MemberType AliasProperty -Name TableName -Value Table
-    $data[$i] | Add-Member -MemberType AliasProperty -Name ColumnName -Value Column
-    $data[$i] | Add-Member -MemberType NoteProperty -Name 'InstanceId' -Value $instanceId
-    $data[$i] | Add-Member -MemberType NoteProperty -Name 'DatabaseName' -Value $databaseName
-    $data[$i] | Add-ClassificationColumnTag -category 'Information Type' -tags @($data[$i].'Information Type')
-    $data[$i] | Add-ClassificationColumnTag -category 'Sensitivity' -tags @($data[$i].'Sensitivity Label')
+    $column = ($columns | Where-Object { $_.tableName -eq $data[$i].Table -and $_.columnName -eq $data[$i].Column})[0]
+
+    $column | Add-ClassificationColumnTag -category 'Information Type' -tags @($data[$i].'Information Type')
+    $column | Add-ClassificationColumnTag -category 'Sensitivity' -tags @($data[$i].'Sensitivity Label')
 }
