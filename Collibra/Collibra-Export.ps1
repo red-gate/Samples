ad3fadc3733416c80ef58bb601ccc546d4296d53
Praveen Dhanasekaran <praveen.dhanasekaran@red-gate.com>
2020-04-01 16:36:39 +0100

    move function to top]

--- Collibra/Collibra-Export.ps1
+++ Collibra/Collibra-Export.ps1
@@ -9,21 +9,6 @@ $collibraPassword = "[collibra user password]"
 . ".\convert-to-collibra.ps1"
 . ".\collibra-api.ps1"
 
-Invoke-WebRequest -Uri "$serverUrl/powershell" -OutFile './data-catalog.psm1' -Headers @{"Authorization" = "Bearer $authToken" }
-
-Import-Module .\data-catalog.psm1 -Force
-
-# connect to your SQL Data Catalog instance - you'll need to generate an auth token in the UI
-Connect-SqlDataCatalog -AuthToken $authToken -ServerUrl $serverUrl
-Connect-Collibra -collibraAPI $collibraAPI -userName $collibraUserName  -password $collibraPassword
-
-Get-ClassificationInstance
-| ForEach-Object { Get-ClassificationDatabase -InstanceName $_.Name }
-| ForEach-Object { export-database -instanceName $_.instanceName -databaseName $_.name }
-
-Complete-CollibraSync -synchronizationId $synchronizationId
-Disconnect-Collibra
-
 function export-database(
     [Parameter(Mandatory)][string] $instanceName,
     [Parameter(Mandatory)][string] $databaseName
@@ -40,3 +25,18 @@ function export-database(
 
     Import-BatchCollibraDatabase -synchronizationId $synchronizationId -json $exportJson
 }
+
+Invoke-WebRequest -Uri "$serverUrl/powershell" -OutFile './data-catalog.psm1' -Headers @{"Authorization" = "Bearer $authToken" }
+
+Import-Module .\data-catalog.psm1 -Force
+
+# connect to your SQL Data Catalog instance - you'll need to generate an auth token in the UI
+Connect-SqlDataCatalog -AuthToken $authToken -ServerUrl $serverUrl
+Connect-Collibra -collibraAPI $collibraAPI -userName $collibraUserName  -password $collibraPassword
+
+Get-ClassificationInstance
+| ForEach-Object { Get-ClassificationDatabase -InstanceName $_.Name }
+| ForEach-Object { export-database -instanceName $_.instanceName -databaseName $_.name }
+
+Complete-CollibraSync -synchronizationId $synchronizationId
+Disconnect-Collibra
