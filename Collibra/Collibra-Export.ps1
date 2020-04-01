657c3ae6fea844c7975f30b007deb3c464ddf1d6
Praveen Dhanasekaran <praveen.dhanasekaran@red-gate.com>
2020-04-01 14:27:30 +0100

    Export entire estate

--- Collibra/Collibra-Export.ps1
+++ Collibra/Collibra-Export.ps1
@@ -1,8 +1,6 @@
 $authToken = "[Your auth token]"
 $serverUrl = "http://[Your SQL Data Catalog Server FQDN]:15156" # or https:// if you've configured SSL
-$instanceName = 'sql-server1.domain.com'
-$databaseName = 'AdventureWorks'
-$communityName =  'RedgateSqlDataCatalog'
+$communityName = 'RedgateSqlDataCatalog'
 $synchronizationId = 'RedgateSqlDataCatalog'
 $collibraAPI = 'https://redgate.collibra.com/rest/2.0'
 $collibraUserName = "[collibra user name here]"
@@ -17,19 +15,28 @@ Import-Module .\data-catalog.psm1 -Force
 
 # connect to your SQL Data Catalog instance - you'll need to generate an auth token in the UI
 Connect-SqlDataCatalog -AuthToken $authToken -ServerUrl $serverUrl
-
-# get all columns into a collection
-$allColumns = Get-ClassificationColumn -instanceName $instanceName -databaseName $databaseName
-
-if (-not $allColumns) {
-    throw "No columns returned, check if the instance '$instanceName' is" +
-    "registered and that it contains a database '$databaseName'."
-}
-
-$exportJson = Convert-CollibraJSON -communityName $communityName -instanceName  $instanceName -databaseName $databaseName -columns $allColumns
-
 Connect-Collibra -collibraAPI $collibraAPI -userName $collibraUserName  -password $collibraPassword
 
-Import-CollibraDatabase -synchronizationId $synchronizationId -json $exportJson
+Get-ClassificationInstance
+| ForEach-Object { Get-ClassificationDatabase -InstanceName $_.Name }
+| ForEach-Object { export-database -instanceName $_.instanceName -databaseName $_.name }
 
+Complete-CollibraSync -synchronizationId $synchronizationId
 Disconnect-Collibra
+
+function export-database(
+    [Parameter(Mandatory)][string] $instanceName,
+    [Parameter(Mandatory)][string] $databaseName
+) {
+    # get all columns into a collection
+    $allColumns = Get-ClassificationColumn -instanceName $instanceName -databaseName $databaseName
+
+    if (-not $allColumns) {
+        throw "No columns returned, check if the instance '$instanceName' is" +
+        "registered and that it contains a database '$databaseName'."
+    }
+
+    $exportJson = Convert-CollibraJSON -communityName $communityName -instanceName  $instanceName -databaseName $databaseName -columns $allColumns
+
+    Import-BatchCollibraDatabase -synchronizationId $synchronizationId -json $exportJson
+}
--- Collibra/import-collibra.ps1
+++ Collibra/import-collibra.ps1
@@ -11,7 +11,7 @@ function Connect-Collibra {
         username = $userName;
         password = $password
     } | ConvertTo-Json
-    Invoke-RestMethod -Uri $uri -Method "Post" -Body $body -ContentType 'application/json' -SessionVariable 'Session'
+    Invoke-RestMethod -Uri $uri -Method Post -Body $body -ContentType 'application/json' -SessionVariable 'Session'
 
     $Script:session = $Session
     $Script:collibaUri = $collibraAPI
@@ -31,8 +31,31 @@ function Import-CollibraDatabase {
     Invoke-RestMethod -Uri $Uri -Method Post -Form $Form -WebSession $Script:session -ContentType 'multipart/form-data'
 }
 
+function Import-BatchCollibraDatabase {
+    param (
+        [Parameter(Mandatory)][string] $synchronizationId,
+        [Parameter(Mandatory, ValueFromPipeLine)][object] $json
+    )
+    $Uri = "$Script:collibaUri/import/synchronize/$synchronizationId/batch/json-job"
+    $json | ConvertTo-Json -Depth 10 | Out-File '.\export.json'
+    $Form = @{
+        fileName = 'import_file'
+        file     = Get-Item -Path  '.\export.json'
+    }
+    Invoke-RestMethod -Uri $Uri -Method Post -Form $Form -WebSession $Script:session -ContentType 'multipart/form-data'
+}
+
+function Complete-CollibraSync {
+    param (
+        [Parameter(Mandatory)][string] $synchronizationId
+    )
+    $Uri = "$Script:collibaUri/import/synchronize/$synchronizationId/finalize/job"
+    Invoke-RestMethod -Uri $uri -Method Post -WebSession $Script:session
+    $Script:session = $null
+}
+
 function Disconnect-Collibra {
     $uri = "$Script:collibaUri/auth/sessions/current"
-    Invoke-RestMethod -Uri $uri -Method "Delete" -WebSession $Script:session
+    Invoke-RestMethod -Uri $uri -Method Delete -WebSession $Script:session
     $Script:session = $null
 }
