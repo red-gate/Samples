a6ba43642eb7a36dcdfbc39a34e822453f2289f8
twalsh92 <twalsh92@users.noreply.github.com>
2021-11-15 14:30:05 +0000

    WIP

--- SqlDataCatalog/Add descoping rule.ps1
+++ SqlDataCatalog/Add descoping rule.ps1
@@ -1,14 +1,17 @@
 # Based on the structure of AdventureWorks (see `Classify Adventureworks database`)
 # Presumes that you have the foundational information types and information classification tags
 
-$authToken = "[Your auth token]"
-$serverUrl = "http://[Your SQL Data Catalog Server FQDN]:15156" # or https:// if you've configured SSL
+$authToken = "OTA5ODA3NTU3MDM0Mzc3MjE2OjgzNzA1OTMyLWMxMDktNDYwNi05NzM1LWI1MmM1Y2MxMjQ5ZA=="
+$serverUrl = "http://localhost:15156" # or https:// if you've configured SSL
 
 # Load SQL Data Catalog PowerShell Module
 Invoke-WebRequest -Uri "$serverUrl/powershell" -OutFile 'data-catalog.psm1' `
 -Headers @{"Authorization" = "Bearer $authToken" }
 
 Import-Module .\data-catalog.psm1 -Force
+
+# Connect to your SQL Data Catalog instance - you'll need to generate an auth token in the UI
+Connect-SqlDataCatalog -AuthToken $authToken -ServerUrl $serverUrl
 
 $body = @{
     name     = "ZZ. Descope system columns"
@@ -41,4 +44,4 @@ $body = @{
     )
 }
 
-Invoke-WebRequest -Uri "$serverUrl/api/v1.0/suggestion-rules" -Method Post -Body $body
+Invoke-RestMethod -Uri "$serverUrl/api/v1.0/suggestion-rules" -Method Post -Body $body -ContentType 'application/json; charset=utf-8' -UseBasicParsing -Headers @{"Authorization" = "Bearer $authToken"}
