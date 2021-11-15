764e524ca176378694c057940d0f9ed49672b651
twalsh92 <twalsh92@users.noreply.github.com>
2021-11-15 15:56:18 +0000

    Fix the script for adding descoping rule

--- SqlDataCatalog/Add descoping rule.ps1
+++ SqlDataCatalog/Add descoping rule.ps1
@@ -1,8 +1,8 @@
 # Based on the structure of AdventureWorks (see `Classify Adventureworks database`)
 # Presumes that you have the foundational information types and information classification tags
 
-$authToken = "OTA5ODE2NDgzOTg1NDg5OTIwOjI5MzM3NjQyLWQwZDItNGZiZS1iOGY5LTc0ZDMyYjUyZDcwMQ=="
-$serverUrl = "http://localhost:15156" # or https:// if you've configured SSL
+$authToken = "[your token here]"
+$serverUrl = "http://[Your SQL Data Catalog Server FQDN]:15156" # or https:// if you've configured SSL
 
 $headers = @{
     'Authorization' = "Bearer $authToken"
@@ -13,31 +13,33 @@ $body = @{
     name     = "ZZ. Descope system columns"
     tagIds   = @(491288012315960037, 491288012315962001)
     filters = @(
-        {
-            primaryKeyFilter = "Require",
-            taggedColumnsFilter = "Exclude",
+        @{
+            primaryKeyFilter = "Require"
+            taggedColumnsFilter = "Exclude"
             columnDataTypeFullNames = @("int", "bigint", "tinyint", "smallint", "uniqueidentifier")
         },
-        {
-            foreignKeyFilter = "Require",
-            taggedColumnsFilter = "Exclude",
+        @{
+            foreignKeyFilter = "Require"
+            taggedColumnsFilter = "Exclude"
             columnDataTypeFullNames = @("int", "bigint", "tinyint", "smallint", "uniqueidentifier")
         },
-        {
-            compositeKeyFilter = "Require",
-            taggedColumnsFilter = "Exclude",
+        @{
+            compositeKeyFilter = "Require"
+            taggedColumnsFilter = "Exclude"
             columnDataTypeFullNames = @("int", "bigint", "tinyint", "smallint", "uniqueidentifier")
         },
-        {
-            identityConstraintFilter = "Require",
-            taggedColumnsFilter = "Exclude",
+        @{
+            identityConstraintFilter = "Require"
+            taggedColumnsFilter = "Exclude"
             columnDataTypeFullNames = @("int", "bigint", "tinyint", "smallint", "uniqueidentifier")
         },
-        {
-            taggedColumnsFilter = "Exclude",
+        @{
+            taggedColumnsFilter = "Exclude"
             columnDataTypeFullNames = @("bit", "uniqueidentifier")
         }
     )
 }
 
-Invoke-RestMethod -Uri "$serverUrl/api/v1.0/suggestion-rules" -Method Post -Body ($body | ConvertTo-Json) -ContentType 'application/json; charset=utf-8' -UseBasicParsing -Headers $headers
+$bodyJson = @($body) | ConvertTo-Json -Depth 3
+
+Invoke-RestMethod -Uri "$serverUrl/api/v1.0/suggestion-rules" -Method Post -Body $bodyJson -ContentType 'application/json; charset=utf-8' -UseBasicParsing -Headers $headers
