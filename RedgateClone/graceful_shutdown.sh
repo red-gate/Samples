ce56e78c132cca62b9e550f9e48c2bba60c05b47
Oli Wennell <oli.wennell@red-gate.com>
2024-05-28 13:56:20 +0100

    Adds script to perform graceful shutdown of Redgate Clone cluster

---
+++ RedgateClone/graceful_shutdown.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script can be used to gracefully drain a redgate clone node. This is useful for a reboot or a shutdown.
+
+set -Eeuo pipefail
+
+currentmachine=$(cat /etc/machine-id)
+command="kubectl get nodes -o=jsonpath='{.items[?(@.status.nodeInfo.machineID==\"$currentmachine\")].metadata.name}'"
+
+currentnode=$($command)
+currentnode=$(echo "|$currentnode|" | cut -d "'" -f 2)
+
+echo "Cordon node $currentnode on machine $currentmachine"
+kubectl cordon $currentnode
+
+echo "Drain $currentnode on machine $currentmachine"
+kubectl drain $currentnode --ignore-daemonsets --delete-emptydir-data --skip-wait-for-delete-timeout=60
